e97ead76c1c49b59c74511e11b83463515630f44
Rohit Borse <codingwithrb@gmail.com>
2023-12-10 22:11:20 +0530

    update route added

--- src/controllers/appStore.controller.js
+++ src/controllers/appStore.controller.js
@@ -1,6 +1,7 @@
 import { appStore } from '../models/appStore.model.js'
 import { uploadFileOnCloudinary } from '../utils/cloudinary.js';
 
+//api that add project in database
 const addProject = async (req, res) => {
 
     // fetching and validating request fields 
@@ -26,7 +27,7 @@ const addProject = async (req, res) => {
     const preview_image = await uploadFileOnCloudinary(previewLocalPath);
 
     if (!logo_image) {
-        res.status(500).error("logo image not found")
+        res.status(500).send("logo image not found")
     }
 
     // storing appProject on database 
@@ -42,10 +43,63 @@ const addProject = async (req, res) => {
 
     const checkedApp = await appStore.findById(newApp._id)
     if (!checkedApp) {
-        return res.status(501).error("something went wrong when adding project")
+        return res.status(501).send("something went wrong when adding project")
     }
 
     return res.status(201).json(newApp);
 }
 
-export { addProject }
+//api that update project in database
+const updateProject = async (req, res) => {
+
+    // fetching and validating request fields 
+    const { title, description, category, developer, preview_link } = req.body;
+
+    if (
+        [title, description, category, developer, preview_link].some((field) => field?.trim() === "")
+    ) {
+        return res.status(400).json({
+            "error": "Please Fill Required Fields"
+        })
+    }
+
+    // accessing local file path from request injected by multer middleware
+    const logoLocalPath = req.files?.logo_images[0]?.path;
+    const previewLocalPath = req.files?.preview_images[0]?.path;
+
+    if (!logoLocalPath) {
+        return res.status(400).send("logo image is required")
+    }
+
+    // uploading local files on cloudinary and return files link 
+    const logo_image = await uploadFileOnCloudinary(logoLocalPath);
+    const preview_image = await uploadFileOnCloudinary(previewLocalPath);
+
+    if (!logo_image) {
+        res.status(500).send("logo image not found")
+    }
+
+    const projectId = req.params.id;
+    const updatedProject = {
+        title,
+        description,
+        logo_images: logo_image.url,
+        preview_images: preview_image?.url || "",
+        category,
+        developer,
+        preview_link
+    }
+
+    try {
+        console.log("uploading")
+        const newproject = await appStore.updateOne({ _id: projectId }, updatedProject)
+        return res.send(newproject);
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+//api that delete project in database
+// api that query projects in database 
+
+export { addProject, updateProject }
--- src/routes/appstore.routes.js
+++ src/routes/appstore.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { addProject } from "../controllers/appStore.controller.js";
+import { addProject, updateProject } from "../controllers/appStore.controller.js";
 import { upload } from '../middlewares/multer.middleware.js'
 
 const router = Router();
@@ -14,5 +14,16 @@ router.route("/add-project").post(upload.fields([
         maxCount: 4
     }
 ]), addProject);
+
+router.route("/update-project").put(upload.fields([
+    {
+        name: "logo_images",
+        maxCount: 1
+    },
+    {
+        name: "preview_images",
+        maxCount: 4
+    }
+]), updateProject)
 
 export default router;
